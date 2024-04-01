5499469c38a2b4ea170a4c302dba9b273325df32
Michael-Xie <xiemicha@gmail.com>
2024-04-01 03:04:41 -0400

    feat: get next grid perpetually when start is clicked

--- app/conways-game-of-life/page.tsx
+++ app/conways-game-of-life/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 import { CellGrid } from '@/components/CellGrid/CellGrid';
 import { Button, Container } from '@mantine/core';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getNextGrid } from './_utilities/gameOfLife';
 
 const createBlankGrid = (numRows: number, numCols: number) => {
   return Array(numRows).fill(Array(numCols).fill(false));
@@ -10,7 +11,7 @@ export default function Page() {
   const NUM_COL = 10;
   const NUM_ROW = 10;
   const [gridData, setGridData] = useState(createBlankGrid(NUM_ROW, NUM_COL));
-
+  const [startGeneration, setStartGeneration] = useState(false);
   const handleCellClick = (rowIndex: number, colIndex: number) => {
     const newGridData = gridData.map((row, i) =>
       i === rowIndex ? row.map((col: boolean, j: number) => (j === colIndex ? !col : col)) : row
@@ -20,15 +21,27 @@ export default function Page() {
 
   const handleResetCellGrid = () => {
     setGridData(createBlankGrid(NUM_ROW, NUM_COL));
+    setStartGeneration(false);
   };
 
-  const handleClick = (e) => {
-    console.log(e);
+  const handleStartToggle = () => {
+    setStartGeneration(!startGeneration);
   };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (startGeneration) {
+        setGridData(getNextGrid(gridData));
+      }
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [gridData, startGeneration]);
+
   return (
     <Container>
       <h1>Hello, Conway's Game of Life page!</h1>
-      <Button onClick={handleClick}>Start</Button>
+      <Button onClick={handleStartToggle}>{startGeneration ? 'Pause' : 'Start'}</Button>
       <Button onClick={handleResetCellGrid}>Reset</Button>
       <CellGrid gridData={gridData} onCellClick={handleCellClick} />
     </Container>
